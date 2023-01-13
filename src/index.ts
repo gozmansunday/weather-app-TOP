e5bd5db3bda06389a86e864ca4cc1728ec16475e
Gozman <thegzmn@gmail.com>
2023-01-13 23:13:32 +0100

    Update function to get current weather

--- src/index.ts
+++ src/index.ts
@@ -77,10 +77,17 @@ const CreateCurrentWeatherDetailsObj = (weatherDetails: CurrentWeatherDetails): 
 
 const getCityDetails = async (cityName: string): Promise<CityDetails> => {
   try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=9095cc5220ce63f359ff2704300c35ba`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=9095cc5220ce63f359ff2704300c35ba`, { mode: 'cors' });
     const citiesList = await response.json();
+    const countryFullname = new Intl.DisplayNames(['en'], { type: 'region' });
 
-    const cityDetails = CreateCityDetailsObj({ cityName: citiesList[0].name, state: citiesList[0].state, country: citiesList[0].country, lat: citiesList[0].lat, lon: citiesList[0].lon });
+    const cityDetails = CreateCityDetailsObj({
+      cityName: citiesList[0].name,
+      state: citiesList[0].state,
+      country: countryFullname.of(citiesList[0].country),
+      lat: citiesList[0].lat,
+      lon: citiesList[0].lon
+    });
 
     return cityDetails;
   } catch (error) {
@@ -90,13 +97,28 @@ const getCityDetails = async (cityName: string): Promise<CityDetails> => {
 
 const getCurrentWeather = async (cityName: string): Promise<CurrentWeatherDetails> => {
   const cityDetails = await getCityDetails(cityName);
-
   console.log(cityDetails); //!REMOVE LATER!
+
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityDetails.lat}&lon=${cityDetails.lon}&units=metric&appid=9095cc5220ce63f359ff2704300c35ba`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityDetails.lat}&lon=${cityDetails.lon}&units=metric&appid=9095cc5220ce63f359ff2704300c35ba`, { mode: 'cors' });
     const currentWeatherInfo = await response.json();
 
-    const currentWeatherDetails = CreateCurrentWeatherDetailsObj({ dataCalcTime: currentWeatherInfo.dt, currentTemp: currentWeatherInfo.main.temp, feelsLikeTemp: currentWeatherInfo.main.feels_like, maxTemp: currentWeatherInfo.main.temp_max, minTemp: currentWeatherInfo.main.temp_min, humidity: currentWeatherInfo.main.humidity, pressure: currentWeatherInfo.main.pressure, sunrise: currentWeatherInfo.sys.sunrise, sunset: currentWeatherInfo.sys.sunset, visibility: currentWeatherInfo.visibility, windSpeed: currentWeatherInfo.wind.speed, mainWeather: currentWeatherInfo.weather[0].main, weatherDescription: currentWeatherInfo.weather[0].description, weatherIcon: currentWeatherInfo.weather[0].icon });
+    const currentWeatherDetails = CreateCurrentWeatherDetailsObj({
+      dataCalcTime: currentWeatherInfo.dt,
+      currentTemp: currentWeatherInfo.main.temp,
+      feelsLikeTemp: currentWeatherInfo.main.feels_like,
+      maxTemp: currentWeatherInfo.main.temp_max,
+      minTemp: currentWeatherInfo.main.temp_min,
+      humidity: currentWeatherInfo.main.humidity,
+      pressure: currentWeatherInfo.main.pressure,
+      sunrise: currentWeatherInfo.sys.sunrise,
+      sunset: currentWeatherInfo.sys.sunset,
+      visibility: currentWeatherInfo.visibility,
+      windSpeed: currentWeatherInfo.wind.speed,
+      mainWeather: currentWeatherInfo.weather[0].main,
+      weatherDescription: currentWeatherInfo.weather[0].description,
+      weatherIcon: currentWeatherInfo.weather[0].icon
+    });
 
     console.log(currentWeatherDetails); //! REMOVE LATER!
     return currentWeatherDetails;
@@ -105,4 +127,4 @@ const getCurrentWeather = async (cityName: string): Promise<CurrentWeatherDetail
   }
 };
 
-getCurrentWeather('Owerri');
+getCurrentWeather('new delhi');
