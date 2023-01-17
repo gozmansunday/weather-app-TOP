af78ca8581a9c83fc096f890646d457bf197743e
Gozman <thegzmn@gmail.com>
2023-01-17 23:09:15 +0100

    Update local storage with theme preference

--- src/modules/theme-toggle.ts
+++ src/modules/theme-toggle.ts
@@ -1,17 +1,40 @@
 const html = document.querySelector('html') as HTMLHtmlElement;
 const themeToggleBtn = document.querySelector('#theme-toggle-btn') as HTMLButtonElement;
 
-const toggleBtn = (): void => {
+export default function toggleBtn(): void {
+  window.onload = () => {
+    document.body.classList.remove('transition');
+  };
+
+  let theme: string;
+
+  if (localStorage.getItem('theme')) {
+    theme = JSON.parse(localStorage.getItem('theme'));
+  } else {
+    theme = 'light';
+  }
+
+  if (theme === 'light') {
+    themeToggleBtn.classList.remove('theme-toggle--toggled');
+    html.classList.remove('dark');
+  } else if (theme === 'dark') {
+    themeToggleBtn.classList.add('theme-toggle--toggled');
+    html.classList.add('dark');
+  }
+
+  localStorage.setItem('theme', JSON.stringify(theme));
+
   themeToggleBtn.onclick = () => {
     themeToggleBtn.classList.toggle('theme-toggle--toggled');
     
     if (themeToggleBtn.classList.contains('theme-toggle--toggled')) {
       html.classList.add('dark');
+      theme = 'dark';
     } else {
       html.classList.remove('dark');
+      theme = 'light';
     }
+
+    localStorage.setItem('theme', JSON.stringify(theme));
   };
-
 };
-
-export default toggleBtn;
